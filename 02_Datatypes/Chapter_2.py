5e9c70f8c851b11dcb5c76066328ea918bab3ee3
Anish-Sharma24 <learninghowtoprogramme@gmail.com>
2025-10-19 18:45:56 +0530

    Mutable example..

---
+++ 02_Datatypes/Chapter_2.py
@@ -0,0 +1,7 @@
+spice_mix = set ()
+print (f"Initial spice_mix ID: {id(spice_mix)}")
+print (f"Initial spice_mix ID: {spice_mix}")
+spice_mix.add ("Ginger")
+spice_mix.add ("Cardamom")
+print (f"Initial spice_mix ID: {spice_mix}")
+print (f"After spice_mix ID: {id(spice_mix)}")

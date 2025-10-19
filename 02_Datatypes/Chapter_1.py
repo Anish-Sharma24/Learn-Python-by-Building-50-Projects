910e7ba811dda39e293dc7a90913330d93858d5d
Anish-Sharma24 <learninghowtoprogramme@gmail.com>
2025-10-19 18:30:50 +0530

    Immutable example..

---
+++ 02_Datatypes/Chapter_1.py
@@ -0,0 +1,7 @@
+sugar_amount = 52
+print (f"Initial sugar amount is {sugar_amount}")
+sugar_amount = 20
+print (f"Second sugar amout is {sugar_amount}")
+print ("===============================================================================")
+print (f"ID of 52 is {id(52)}")
+print (f"ID of 20 is {id(20)}")
